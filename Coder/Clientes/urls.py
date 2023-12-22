96e335b208d9e8ea4c58c57eb59640c4117aa237
LihuenG <lihuengimenez@gmail.com>
2023-12-21 21:27:07 -0300

    Trabajo final

--- Coder/Clientes/urls.py
+++ Coder/Clientes/urls.py
@@ -4,6 +4,6 @@ from Clientes.views import cliente_nuevo, producto, envio
 urlpatterns = [
     path('cliente_nuevo/', cliente_nuevo, name='cliente_nuevo'),
     path('producto/', producto, name='producto'),
-    path('envio/', envio, name='envio')
-    
+    path('envio/', envio, name='envio'),
+
 ]
